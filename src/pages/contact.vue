556b374b62bc2d90d684db7e563dc2a5bfd7fb27
carlos capacho <carlos.gomes349@gmail.com>
2021-07-31 14:30:20 -0400

    change ubication

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -16,7 +16,7 @@
                     <div class="col-md-6">
                         <div class="card-information-contact">
                             <h3>Información de contacto</h3>
-                            <p>Direción: <a href="https://goo.gl/maps/GhvcDBH1ppBDae1KA" target="_blank">Av. Pedro de Valdivia 3474 Caracol Ñuñoa, Local 53-B Ñuñoa, Chile</a></p>
+                            <p>Direción: <a href="https://goo.gl/maps/8vNLYeF4Gn6zsqLZ6" target="_blank">Av. Pedro de Valdivia 3464, Local 25B, Ñuñoa, Región Metropolitana</a></p>
                             <br>
                             <p>Teléfono: <a href="https://wa.me/56972628949" target="_blank">+56 9 7262 8949</a></p>
                             <br>
@@ -62,7 +62,7 @@
                 </div>
             </div>
         </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d832.1996603971388!2d-70.60527437074302!3d-33.454552198798325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf9158f180b7%3A0x2f4472985a51de08!2sCaracol%20%C3%91u%C3%B1oa%20Centro!5e0!3m2!1ses!2sve!4v1594407454244!5m2!1ses!2sve" style="width:100%" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.8013861085406!2d-70.60687718523621!3d-33.454480705104395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf6a66275773%3A0x71ed1bd3a3b0ed89!2sKK%20Pretty%20Nails!5e0!3m2!1ses-419!2scl!4v1627755991619!5m2!1ses-419!2scl" style="width:100%" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         <modal :show.sync="modals.alert.show"
              :class="modals.alert.type"
              :show-close="false"
